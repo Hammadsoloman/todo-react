2969dcce63fe557e84fa4888128bda4edd0655d2
Hammadsoloman <hammadali95@yahoo.com>
2020-07-11 13:12:27 +0300

    Updates

---
+++ precache-manifest.f13e678c22b27d3ed85e58e7747efa3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd7142c6f93e910750f9c1213626e22",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "b5857fd7a0cb92436407",
+    "url": "/todo-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "71c8995c4f962f945307",
+    "url": "/todo-react/static/css/main.13a2a290.chunk.css"
+  },
+  {
+    "revision": "b5857fd7a0cb92436407",
+    "url": "/todo-react/static/js/2.e4d24313.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todo-react/static/js/2.e4d24313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71c8995c4f962f945307",
+    "url": "/todo-react/static/js/main.48b6e735.chunk.js"
+  },
+  {
+    "revision": "8b8439a1bcfb1adefc93",
+    "url": "/todo-react/static/js/runtime-main.07f84ba9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a4d073e0c7e5faf26d482d232a316aa9.js"
+  "/todo-react/precache-manifest.f13e678c22b27d3ed85e58e7747efa3d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
