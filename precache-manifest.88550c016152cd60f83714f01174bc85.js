e025d1c10d06b1bd43a55ca50e943566216f16f6
Hammadsoloman <hammadali95@yahoo.com>
2020-07-17 02:15:31 +0300

    Updates

---
+++ precache-manifest.88550c016152cd60f83714f01174bc85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b138aebcd8b097ea81c987d6360a3261",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "5802ea4a92451f610593",
+    "url": "/todo-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b93f86eed56de5344182",
+    "url": "/todo-react/static/css/main.325cc973.chunk.css"
+  },
+  {
+    "revision": "5802ea4a92451f610593",
+    "url": "/todo-react/static/js/2.3a6d19fb.chunk.js"
+  },
+  {
+    "revision": "d3286c608b7b6d68fb79cf25ce73f2fb",
+    "url": "/todo-react/static/js/2.3a6d19fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b93f86eed56de5344182",
+    "url": "/todo-react/static/js/main.a2ec72f7.chunk.js"
+  },
+  {
+    "revision": "8b8439a1bcfb1adefc93",
+    "url": "/todo-react/static/js/runtime-main.07f84ba9.js"
+  }
+]);
